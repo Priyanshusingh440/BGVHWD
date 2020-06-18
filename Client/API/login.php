90999476ef2cc68f96ea13f5e0928ceadb1ea361
unknown <Priyanshusingh541@gmail.com>
2020-06-18 13:23:58 +0530

    first commit

--- Client/API/login.php
+++ Client/API/login.php
@@ -1,10 +1,17 @@
 <?php
 
+$json_data = file_get_contents("php://input");
+
+$data = json_decode($json_data, true);
+
+
+
+
 
 require_once "../config/config.php";
 
 #---------------------<---Code Written and Designed By Priyanshu Raghuvanshi--->-----------------------#
-
+/*
 $get_connection=new connectdb;
 $db=$get_connection->connect();
 
@@ -21,16 +28,15 @@ class login
  
     
 $json_data = file_get_contents("php://input");
-$data = json_decode($json_data, true);
-var_dump($data);
 
-/*
-if(!empty($json_data))
+
+// Checks if it's empty or not
+if( !empty($json_data) )
 {
   
-  
+  // Decodes the JSON object to an Array
   $data = json_decode($json_data, true);
-  var_dump($data);
+  
   $uname=$data['name'];
   $password=$data['pwd'];
   
@@ -38,7 +44,6 @@ if(!empty($json_data))
         if ($uname != "" && $password != "")
         {
             $check='SELECT * FROM client  WHERE email="'.$uname.'" and password="'.$password.'"';
-                
             $result=$this->conn->query($check);
             
             if($result->num_rows==1)
@@ -64,13 +69,12 @@ else
 {
   echo "Empty JSON object, something's wrong!";
 }
-*/
+
                 }
 
             }
- 
+
             $basic_details=new login($db);
             $basic_details->update_details();
 
-        
-            ?>
+            */
