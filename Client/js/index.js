b9e675fafa64c8d85d5bd2401fea18b15faa7b5d
sandipsaha597 <sandipsaha597@gmail.com>
2020-06-26 14:05:50 +0530

    modal when wrong credentials in client

--- Client/js/index.js
+++ Client/js/index.js
@@ -1,5 +1,7 @@
 
 document.getElementById('postform').addEventListener('submit',postname);
+let modalLabel2 = document.querySelector(".new-modal #exampleModalLabel")
+let modalLaunchButton2 = document.querySelector(".new-modal .launch")
 
 function postname(e){
     e.preventDefault();
@@ -26,11 +28,12 @@ function postname(e){
 			if(this.responseText==1)
 			{
 					window.location.href="home.php";
-			}
+			} 
 			else if(this.responseText==0)
 			{
-				
-				alert("Wrong Details");
+				// alert("Wrong Details");
+				modalLabel2.innerHTML = "Wrong Details"
+				modalLaunchButton2.click()
 			}
 		
 			
